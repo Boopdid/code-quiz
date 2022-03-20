663c1c5afe3a488129201367b855c7f5e76cce29
Peter Vrutneski <pvrutneski@gmail.com>
2022-03-19 20:54:57 -0600

    added finish section and added switch staement to go thru questions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,4 @@
+var score = 0;
 var startQuiz = document.getElementById("startQuiz");
 startQuiz.addEventListener("click", function () {
   var startSection = document.getElementById("start");
@@ -9,8 +10,39 @@ startQuiz.addEventListener("click", function () {
 
 var selectedAnswer = document.querySelectorAll(".answer");
 selectedAnswer.forEach(function (answer) {
-  answer.addEventListener("click", function () {
-    console.log("You selected an answer.");
+  answer.addEventListener("click", function (event) {
+    console.log("You selected an answer.", event);
+    var buttonClasses = event.target.classList;
+    console.log(buttonClasses);
+    var currentQuestion = event.target.parentElement.id;
+    console.log(currentQuestion);
+    var nextSection = "";
+    switch (currentQuestion) {
+      case "first":
+        nextSection = "second";
+        break;
+      case "second":
+        nextSection = "third";
+        break;
+      case "third":
+        nextSection = "fourth";
+        break;
+      case "fourth":
+        nextSection = "fifth";
+        break;
+      case "fifth":
+        nextSection = "finish";
+        break;
+    }
+    event.target.parentElement.classList.add("hidden");
+    var nextElement = document.getElementById(nextSection);
+    nextElement.classList.remove("hidden");
+    if (buttonClasses.contains("right")) {
+      console.log("You got it right");
+      score += 10;
+    } else {
+      console.log("you got it wrong");
+    }
   });
 });
 

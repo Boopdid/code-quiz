589e5730e940e6391d354af20a60ee1e2909761f
Peter Vrutneski <pvrutneski@gmail.com>
2022-03-19 23:05:50 -0600

    Moved timer and view score to header, add view scores section, add event listeners for view scores, and style quiz

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -17,13 +17,15 @@ startQuiz.addEventListener("click", function () {
       time -= 1;
       setTimerText(time);
     } else {
-      console.log("Hello");
-      var visibleSection = document.getElementById(sectionShowing);
-      visibleSection.classList.add("hidden");
-      var finishedSection = document.getElementById("finish");
-      finishedSection.classList.remove("hidden");
-      var scoreElement = document.getElementById("finalScore");
-      scoreElement.innerHTML = score;
+      if (sectionShowing != "scores") {
+        var visibleSection = document.getElementById(sectionShowing);
+        visibleSection.classList.add("hidden");
+        var finishedSection = document.getElementById("finish");
+        finishedSection.classList.remove("hidden");
+        sectionShowing = "finish";
+        var scoreElement = document.getElementById("finalScore");
+        scoreElement.innerHTML = score;
+      }
     }
   }, 1000);
   var startSection = document.getElementById("start");
@@ -31,7 +33,6 @@ startQuiz.addEventListener("click", function () {
   var firstQuestion = document.getElementById("first");
   firstQuestion.classList.remove("hidden");
   sectionShowing = "first";
-  console.log("Ouch");
 });
 
 var selectedAnswer = document.querySelectorAll(".answer");
@@ -80,5 +81,60 @@ selectedAnswer.forEach(function (answer) {
     }
   });
 });
-var submitButton = document.getElementById("submitIntials");
-submitButton.addEventListener("click", function () {});
+var submitButton = document.getElementById("submitInitials");
+submitButton.addEventListener("click", function () {
+  var initials = document.getElementById("initials").value;
+  var scores = JSON.parse(localStorage.getItem("scores")) || [];
+  console.log(initials, scores);
+  scores = scores.concat({
+    score: score,
+    initials: initials,
+  });
+  localStorage.setItem("scores", JSON.stringify(scores));
+  var finishSection = document.getElementById("finish");
+  finishSection.classList.add("hidden");
+  var scoreSection = document.getElementById("scores");
+  scoreSection.classList.remove("hidden");
+  sectionShowing = "scores";
+  var scoresList = document.getElementById("scorelist");
+  scoresList.innerHTML = "";
+  scores.forEach(function (score) {
+    var li = document.createElement("li");
+    li.innerHTML = `${score.initials}: ${score.score}`;
+    scoresList.appendChild(li);
+  });
+});
+var returnButton = document.getElementById("returnStart");
+returnButton.addEventListener("click", function () {
+  var scoreSection = document.getElementById("scores");
+  scoreSection.classList.add("hidden");
+  var startSection = document.getElementById("start");
+  startSection.classList.remove("hidden");
+  sectionShowing = "start";
+});
+
+var clearScoresButton = document.getElementById("clearScores");
+clearScoresButton.addEventListener("click", function () {
+  localStorage.clear();
+  var scoresList = document.getElementById("scorelist");
+  scoresList.innerHTML = "";
+});
+
+var viewScoreButton = document.getElementById("viewScores");
+viewScoreButton.addEventListener("click", function () {
+  time = 0;
+  setTimerText(0);
+  var visibleSection = document.getElementById(sectionShowing);
+  visibleSection.classList.add("hidden");
+  var scoreSection = document.getElementById("scores");
+  scoreSection.classList.remove("hidden");
+  sectionShowing = "scores";
+  var scoresList = document.getElementById("scorelist");
+  var scores = JSON.parse(localStorage.getItem("scores")) || [];
+  scoresList.innerHTML = "";
+  scores.forEach(function (score) {
+    var li = document.createElement("li");
+    li.innerHTML = `${score.initials}: ${score.score}`;
+    scoresList.appendChild(li);
+  });
+});

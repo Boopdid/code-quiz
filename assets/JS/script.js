547b67d7d97c738607b720b2ba8f1c2f984d2209
Peter Vrutneski <pvrutneski@gmail.com>
2022-03-19 18:21:21 -0600

    added .hidden class in css and adde d JavaScript to hide quiz start and populate first question

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,9 @@
 var startQuiz = document.getElementById("startQuiz");
 startQuiz.addEventListener("click", function () {
+  var startSection = document.getElementById("start");
+  startSection.classList.add("hidden");
+  var firstQuestion = document.getElementById("first");
+  firstQuestion.classList.remove("hidden");
   console.log("Ouch");
 });
 
